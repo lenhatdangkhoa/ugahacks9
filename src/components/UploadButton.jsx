5cf07fdbd431408b2a546260110dc376da784c15
Pwang393 <Pwang393@gatech.edu>
2024-02-09 22:38:42 -0500

    Make is so you can only upload 1 file

--- src/components/UploadButton.jsx
+++ src/components/UploadButton.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import { UploadOutlined } from '@ant-design/icons';
-import { Button, message, Upload } from 'antd';
-const props = {
-  name: 'file',
-  action: 'https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188',
-  headers: {
-    authorization: 'authorization-text',
-  },
-  onChange(info) {
-    if (info.file.status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (info.file.status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully`);
-    } else if (info.file.status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-};
-const UploadButton = () => (
-  <Upload {...props}>
-    <Button icon={<UploadOutlined />}>Click to Upload</Button>
-  </Upload>
+import { Button, Space, Upload } from 'antd';
+const App = () => (
+  <Space
+    direction="vertical"
+    style={{
+      width: '100%',
+    }}
+    size="large"
+  >
+    <Upload
+      action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+      listType="picture"
+      maxCount={1}
+    >
+      <Button icon={<UploadOutlined />}>Upload (Max: 1)</Button>
+    </Upload>
+  </Space>
 );
-export default UploadButton;
+export default App;
