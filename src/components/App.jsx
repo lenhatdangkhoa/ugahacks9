15382ae9e64feb6854ac77604477742aab4ceb1b
Darren Tham <darrent9859@gmail.com>
2024-02-10 19:29:34 -0500

    Watermark image

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Theme } from "@swc-react/theme";
 import { Slider } from "antd";
 import "@spectrum-web-components/theme/express/scale-medium.js";
@@ -10,76 +10,84 @@ import "./App.css";
 import { Button } from "@swc-react/button";
 
 const App = ({ addOnUISdk }) => {
-  /* 
-This application generates a watermark template
- according to the user's input.
-taking in a text and image parameter. 
-The user can also adjust the size of the text
- */
-  const [watermark, setWatermark] = useState("");
-  const [textSize, setTextSize] = useState(12);
-  const [image, setImage] = useState(null);
-  const watermarkCanvas = useRef(null);
+    /*
+  This application generates a watermark template
+   according to the user's input.
+  taking in a text and image parameter.
+  The user can also adjust the size of the text
+   */
+    const [watermark, setWatermark] = useState("");
+    const [textSize, setTextSize] = useState(12);
+    const [image, setImage] = useState(null);
+    const [width, setWidth] = useState(0)
+    const [height, setHeight] = useState(0)
 
-  function generateWatermark() {
-    const ctx = watermarkCanvas.current.getContext("2d");
-    watermarkCanvas.current.width = 300;
-    watermarkCanvas.current.height = 360;
-    ctx.fillStyle = "black";
-    ctx.fillStyle = "black";
-    let size = textSize.toString();
-    ctx.font = size + "px Arial";
-    ctx.fillText(watermark, 10, 50);
-  }
+    useEffect(() => {
+        async function setDim() {
+            const pages = await addOnUISdk.app.document.getPagesMetadata({ range: addOnUISdk.constants.Range.currentPage })
+            const { width, height } = pages[0].size
+            setWidth(width)
+            setHeight(height)
+        }
+        setDim()
+    }, [])
 
-  async function handleAddToPage() {
-    const blob = await new Promise((resolve, reject) => {
-      watermarkCanvas.current.toBlob((blob) => {
-        resolve(blob);
-      });
-    });
-    addOnUISdk.app.document.addImage(blob);
-  }
-  return (
-    <Theme theme="express" scale="medium" color="light">
-      <div className="container">
-        <div>
-          <h1> Watermark Text</h1>
-          <input
-            type="text"
-            value={watermark}
-            onChange={({ target: { value: e } }) => {
-              setWatermark(e);
-            }}
-          />
-        </div>
-        <div className="text-slider">
-          <h1> Text Size</h1>
-          <h1 className="left"> {textSize}px</h1>
-        </div>
-        <Slider
-          className="slider"
-          defaultValue={textSize}
-          onChange={(value) => setTextSize(value)}
-          tooltip={{
-            open: false,
-          }}
-        />
-        <div>
-          <h1> Icon Upload </h1>
-          <UploadButton />
-        </div>
-        <Preview />
-        <div>
-          <Button onClick={generateWatermark}>Generate</Button>
-          <Button onClick={handleAddToPage}>Add</Button>
-        </div>
-        <div>
-          <canvas ref={watermarkCanvas} />
-        </div>
-      </div>
-    </Theme>
-  );
+    async function handleAddToPage() {
+        const canvas = document.createElement("canvas")
+        const ctx = canvas.getContext("2d")
+        canvas.width = width;
+        canvas.height = height;
+        ctx.fillStyle = "black";
+        ctx.font = `${textSize.toString()}px Arial`
+        ctx.rotate(-30 * Math.PI / 180);
+
+        let str = ""
+        for (let i = 0; i < 20; i++) {
+            str += `${watermark}      `
+        }
+        for (let i = 0; i < 100; i++) {
+            ctx.fillText(str, -width / 2, i * (textSize + 50));
+        }
+
+        canvas.toBlob(blob => addOnUISdk.app.document.addImage(blob))
+    }
+
+    return (
+        <Theme theme="express" scale="medium" color="light">
+            <div className="container">
+                <div>
+                    <h1> Watermark Text</h1>
+                    <input
+                        type="text"
+                        value={watermark}
+                        onChange={({ target: { value: e } }) => {
+                            setWatermark(e);
+                        }}
+                    />
+                </div>
+                <div className="text-slider">
+                    <h1> Text Size</h1>
+                    <h1 className="left"> {textSize}px</h1>
+                </div>
+                <Slider
+                    className="slider"
+                    defaultValue={textSize}
+                    onChange={(value) => setTextSize(value)}
+                    tooltip={{
+                        open: false,
+                    }}
+                />
+                <div>
+                    <h1> Icon Upload </h1>
+                    <UploadButton />
+                </div>
+                <Preview />
+                <div>
+                    <Button onClick={handleAddToPage}>Add</Button>
+                </div>
+            </div>
+        </Theme>
+    );
 };
 
 export default App;
