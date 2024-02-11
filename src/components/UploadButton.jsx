dc953b978f4a600400a7088081293d26e57516a7
Pwang393 <Pwang393@gatech.edu>
2024-02-10 19:45:24 -0500

    Changed upload button

--- src/components/UploadButton.jsx
+++ src/components/UploadButton.jsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import { UploadOutlined } from '@ant-design/icons';
-import { Button, Space, Upload } from 'antd';
-const App = () => (
-  /**
-   * Upload Image Button
-   * Limited to only one input
-   */
-  <Space
-    direction="vertical"
-    style={{
-      width: '100%',
-    }}
-    size="large"
-  >
-    <Upload
-      action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
-      listType="picture"
-      maxCount={1}
-    >
-      <Button icon={<UploadOutlined />}>Upload (Max: 1)</Button>
-    </Upload>
-  </Space>
-);
+import React, { useState } from 'react';
+import { Upload } from 'antd';
+import ImgCrop from 'antd-img-crop';
+const App = () => {
+  const [fileList, setFileList] = useState([
+    {
+      uid: '-1',
+      name: 'image.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    },
+  ]);
+  const onChange = ({ fileList: newFileList }) => {
+    setFileList(newFileList);
+  };
+  const onPreview = async (file) => {
+    let src = file.url;
+    if (!src) {
+      src = await new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file.originFileObj);
+        reader.onload = () => resolve(reader.result);
+      });
+    }
+    const image = new Image();
+    image.src = src;
+    const imgWindow = window.open(src);
+    imgWindow?.document.write(image.outerHTML);
+  };
+  return (
+    <ImgCrop rotationSlider>
+      <Upload
+        action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+        listType="picture-card"
+        maxCount={1}
+        fileList={fileList}
+        onChange={onChange}
+        onPreview={onPreview}
+      >
+        {fileList.length < 5 && '+ Upload'}
+      </Upload>
+    </ImgCrop>
+  );
+};
 export default App;
